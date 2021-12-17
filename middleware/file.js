e6eb173a0001253ea4147e97d081f9bfa5607aaf
t0milov <tomilov.vov-tomilov@yandex.ru>
2021-12-17 17:48:29 +0500

    add video name in post

--- middleware/file.js
+++ middleware/file.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
         cb(null, 'public/')
     },
     filename(req, file, cb){
-        cb(null, new Date().toDateString() + '-' + file.originalname)
+        cb(null, req.params.videoId)
     }
 })
 
--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -2,8 +2,9 @@ const { Router } = require('express')
 const fileMiddeware = require('../middleware/file')
 
 const router = Router()
+// const videoId = ''
 
-router.post('/upload', fileMiddeware.single('fileName'), (req, res) => {
+router.post('/upload/:videoId', fileMiddeware.single('fileName'), (req, res) => {
     try{
         if(req.file){
             res.json(req.file)
